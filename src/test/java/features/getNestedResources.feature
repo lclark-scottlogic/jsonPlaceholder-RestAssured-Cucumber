137a46af26821eefe26f606c19d227fe812a70dd
lclark-scottlogic <lclark@scottlogic.com>
2023-09-25 15:44:52 +0100

    added all scenarios for all endpoints

---
+++ src/test/java/features/getNestedResources.feature
@@ -0,0 +1,67 @@
+Feature: Get All Nested Resources
+  Scenario Outline: Verify we can get a post's comments
+    Given PostId is <postId>
+    When User calls "postComments" endpoint with "GET" method
+    Then Comment array has length 5
+    And Resource at index <index> has "name" with value <name>
+    And Resource at index <index> has "postId" with value <postId>
+    And Resource at index <index> has "email" with value <email>
+    And Resource at index <index> has "body" with value <body>
+    And Resource at index <index> has "id" with value <id>
+    Examples:
+     |index |postId  |id     |name                                    |email               |body                                                                                                                                                                   |
+     |0     |1       |1      |"id labore ex et quam laborum"          |"Eliseo@gardner.biz"|"laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"              |
+     |4     |9       |45     |"autem illo facilis"                    |"Marcia@name.biz"   |"ipsum odio harum voluptatem sunt cumque et dolores\nnihil laboriosam neque commodi qui est\nquos numquam voluptatum\ncorporis quo in vitae similique cumque tempore"  |
+     |3     |5       |24     |"in tempore eos beatae est"             |"Jeffery@juwan.us"  |"repudiandae repellat quia\nsequi est dolore explicabo nihil et\net sit et\net praesentium iste atque asperiores tenetur"                                              |
+  Scenario: Verify we cannot get an invalid post's comments
+    Given PostId is 101
+    When User calls "postComments" endpoint with "GET" method
+    Then Comment array has length 0
+Scenario Outline: Verify we can get a post's photos
+  Given PostId is <postId>
+  When User calls "albumPhotos" endpoint with "GET" method
+  Then Photo array has length 50
+  And Resource at index <index> has "title" with value <title>
+  And Resource at index <index> has "albumId" with value <postId>
+  And Resource at index <index> has "url" with value <url>
+  And Resource at index <index> has "thumbnailUrl" with value <thumbnailUrl>
+  And Resource at index <index> has "id" with value <id>
+  Examples:
+    |index |postId     |id        |title                                                      |url                                       |thumbnailUrl                                    |
+    |36    |2          |87        |"eos nihil sunt accusantium omnis"                         |"https://via.placeholder.com/600/224566"  |"https://via.placeholder.com/150/224566"        |
+    |49    |100        |5000      |"error quasi sunt cupiditate voluptate ea odit beatae"     |"https://via.placeholder.com/600/6dd9cb"  |"https://via.placeholder.com/150/6dd9cb"        |
+    |0     |1          |1         |"accusamus beatae ad facilis cum similique qui sunt"       |"https://via.placeholder.com/600/92c952"  |"https://via.placeholder.com/150/92c952"        |
+  Scenario: Verify we cannot get an invalid post's photos
+    Given PostId is 101
+    When User calls "albumPhotos" endpoint with "GET" method
+    Then Photo array has length 0
+  Scenario Outline: Verify we can get a user's albums
+    Given UserId is <userId>
+    When User calls "userAlbums" endpoint with "GET" method
+    Then Album array has length 10
+    And Resource at index <index> has "title" with value <title>
+    And Resource at index <index> has "id" with value <id>
+    Examples:
+      |index |userId     |id        |title                                                      |
+      |3     |7          |64        |"autem voluptatem amet iure quae"                          |
+      |9     |10         |100       |"enim repellat iste"                                       |
+      |0     |1          |1         |"quidem molestiae enim"                                    |
+  Scenario: Verify we cannot get an invalid user's albums
+    Given UserId is 11
+    When User calls "userAlbums" endpoint with "GET" method
+    Then Album array has length 0
+  Scenario Outline: Verify we can get a user's todos
+    Given UserId is <userId>
+    When User calls "userTodos" endpoint with "GET" method
+    Then Todo array has length 20
+    And Resource at index <index> has "title" with value <title>
+    And Resource at index <index> has "id" with value <id>
+    Examples:
+      |index |userId     |id        |title                                                      |
+      |16    |8          |157       |"dolorem veniam quisquam deserunt repellendus"             |
+      |19    |10         |200       |"ipsam aperiam voluptates qui"                             |
+      |0     |1          |1         |"delectus aut autem"                                       |
+  Scenario: Verify we cannot get an invalid user's albums
+    Given UserId is 11
+    When User calls "userTodos" endpoint with "GET" method
+    Then Todo array has length 0
--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -14,7 +13,11 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.path.json.JsonPath;
 import java.util.HashMap;
 import java.util.Map;
+import org.example.pojo.Album;
+import org.example.pojo.Comment;
+import org.example.pojo.Photo;
 import org.example.pojo.Post;
+import org.example.pojo.ToDo;
 import org.testng.Assert;
 import resources.Api;
 import resources.GetDetails;
@@ -22,71 +25,89 @@ import resources.TestDataBuild;
 import resources.Utils;
 
 public class stepDefinition extends Utils {
-//Variables need to be declared at the start because they need to have
+
+  //Variables need to be declared at the start because they need to have
 //  global scope, so they are reassigned as cucumber step methods run
   RequestSpecification res;
   Response response;
   TestDataBuild data = new TestDataBuild();
   Post post = new Post();
   Post[] postList;
+  Comment[] commentList;
+  Photo[] photoList;
+  Album[] albumList;
+  ToDo[] toDoList;
   int postId;
+  int userId;
+
 
   @Given("New post with {string} {string}")
-  public void new_post_with(String parameter,String value) {
-    res = createPostReq(data.updatePostPayload(parameter,value));
+  public void new_post_with(String parameter, String value) {
+    res = createPostReq(data.updatePostPayload(parameter, value));
   }
 
   @Given("Create post with {int} {int} {string} {string}")
-  public void create_post_with_null(int id, int userId, String title,String body) {
-    res=createPostReq(data.createPostPayload(id,userId,title,body));
+  public void create_post_with_null(int id, int userId, String title, String body) {
+    res = createPostReq(data.createPostPayload(id, userId, title, body));
 
   }
+
   @Given("We set {string} as {string}")
-  public void we_set_as(String parameter,String value){
+  public void we_set_as(String parameter, String value) {
 //    Here we need to inject the json as we cannot create a post with only one field
 //    We use a hashmap which we convert to json to send to the body of the request
-    Map<String,String> payload=new HashMap<>();
-    payload.put(parameter ,value);
+    Map<String, String> payload = new HashMap<>();
+    payload.put(parameter, value);
 //    Convert Payload to JSON
-    ObjectMapper objectMapper=new ObjectMapper();
+    ObjectMapper objectMapper = new ObjectMapper();
     String jsonPayload;
     try {
-      jsonPayload=objectMapper.writeValueAsString(payload);
+      jsonPayload = objectMapper.writeValueAsString(payload);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
       return;
     }
-    res=patchPostReq(jsonPayload);
+    res = patchPostReq(jsonPayload);
   }
+
   @Given("We set {string} as {int}")
-  public void we_set_as(String parameter,Integer value){
+  public void we_set_as(String parameter, Integer value) {
 //    Here we need to inject the json as we cannot create a post with only one field
 //    We use a hashmap which we convert to json to send to the body of the request
-    Map<String, Integer> payload=new HashMap<>();
-    payload.put(parameter ,value);
+    Map<String, Integer> payload = new HashMap<>();
+    payload.put(parameter, value);
 //    Convert Payload to JSON
-    ObjectMapper objectMapper=new ObjectMapper();
+    ObjectMapper objectMapper = new ObjectMapper();
     String jsonPayload;
     try {
-      jsonPayload=objectMapper.writeValueAsString(payload);
+      jsonPayload = objectMapper.writeValueAsString(payload);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
       return;
     }
-    res=patchPostReq(jsonPayload);
+    res = patchPostReq(jsonPayload);
   }
+
   @Given("PostId is {int}")
   public void postIdIsId(int id) {
-    postId=id;
-    res=getPostReq(postId);
+    postId = id;
+    res = getPostReq(postId);
   }
+
+  @Given("UserId is {int}")
+  public void useridIsPostId(int id) {
+    userId = id;
+    res = getPostReq(userId);
+  }
+
   @Given("All posts request")
-  public void allPostsRequest(){
-    res=getAllPostReq();
+  public void allPostsRequest() {
+    res = getAllPostReq();
   }
+
   @Given("User has Id {int}")
-  public void user_has_id(int userId){
-    res=getPostByUserReq(userId);
+  public void user_has_id(int userId) {
+    res = getPostByUserReq(userId);
   }
 
   @When("User calls {string} endpoint with {string} method")
@@ -111,14 +132,16 @@ public class stepDefinition extends Utils {
         response = res.when().post(resourceApi.getResource());
         break;
     }
-}
+  }
+
   @When("User calls GET endpoint for post")
   public void userCallsGetEndpointForPost() {
     response = res.when().get("/posts/{id}");
   }
+
   @Then("Status code is {int}")
   public void status_code_is_int(int code) throws Exception {
-    switch(code) {
+    switch (code) {
       case 200:
         post = response.then().spec(resOk()).extract().response().as(Post.class);
         break;
@@ -126,18 +149,21 @@ public class stepDefinition extends Utils {
         post = response.then().spec(resCreated()).extract().response().as(Post.class);
         break;
       case 404:
-        String error=response.then().spec(resNotFound()).extract().response().asString();
+        String error = response.then().spec(resNotFound()).extract().response().asString();
         break;
       default:
-        throw new Exception("Expected status code: "+code+"\nReceived status code: "+response.getStatusCode());
+        throw new Exception("Expected status code: " + code + "\nReceived status code: "
+            + response.getStatusCode());
 
 
     }
   }
+
   @Then("{string} in response body is {string}")
   public void in_response_body_is_string(String key, String value) {
-      assertEquals(value, post.getJsonString(key));
-    }
+    assertEquals(value, post.getJsonString(key));
+  }
+
   @Then("{string} in response body is:")
   public void in_response_body_is_multiline(String key, String value) {
     assertEquals(value, post.getJsonString(key));
@@ -145,45 +171,78 @@ public class stepDefinition extends Utils {
 
   @Then("{string} in response body is {int}")
   public void in_response_body_is_int(String key, int value) {
-    assertEquals(value,post.getJsonInt(key));
-  }
-//Method which verifies userId using the getuseridfrompostid method
-  @Then("Post belongs to correct user")
-  public void post_belongs_to_correct_user() {
-    assertEquals(GetDetails.getUserIdFromPostId(post.getId()),post.getUserId());
+    assertEquals(value, post.getJsonInt(key));
   }
 
-//Method for getting an element of an array'sw attribute and asserting its value in
+  //Method which verifies userId using the getuseridfrompostid method
+  @Then("Post belongs to correct user")
+  public void post_belongs_to_correct_user() {
+    assertEquals(GetDetails.getUserIdFromPostId(post.getId()), post.getUserId());
+  }
+
+  //Method for getting an element of an array'sw attribute and asserting its value in
 //  the case the value is a string
   @And("Resource at index {int} has {string} with value {string}")
   public void resourceAtIndexHasWithValue(int index, String key, String value) {
     String getAllPosts = response
         .then().spec(resOk()).extract().response().asString();
-    JsonPath js=new JsonPath(getAllPosts);
+    JsonPath js = new JsonPath(getAllPosts);
     String jsonPathExpression = String.format("[%d].%s", index, key);
-    Assert.assertEquals(js.get(jsonPathExpression),value);
+    Assert.assertEquals(js.get(jsonPathExpression), value);
   }
+
   //Method for getting an element of an array'sw attribute and asserting its value in
 //  the case the value is a string
   @Then("Resource at index {int} has {string} with value {int}")
   public void resourceAtIndexHasWithValue(int index, String key, int value) {
     String getAllPosts = response
         .then().spec(resOk()).extract().response().asString();
-    JsonPath js=new JsonPath(getAllPosts);
+    JsonPath js = new JsonPath(getAllPosts);
     String jsonPathExpression = String.format("[%d].%s", index, key);
-    Assert.assertEquals((Integer) js.get(jsonPathExpression),value);
+    Assert.assertEquals((Integer) js.get(jsonPathExpression), value);
   }
-
   @Then("Array has length {int}")
   public void arrayHasLength(int length) {
-    postList= response.then().spec(resOk()).extract().response().as(Post[].class);
-    Assert.assertEquals(postList.length,length);
+    postList = response.then().spec(resOk()).extract().response().as(Post[].class);
+    Assert.assertEquals(postList.length, length);
   }
-//Method verifies all userIds in the list of posts are correct
+
+  @Then("Comment array has length {int}")
+  public void commentArrayHasLength(int length) {
+    commentList = response.then().spec(resOk()).extract().response().as(Comment[].class);
+    Assert.assertEquals(commentList.length, length);
+  }
+
+  @Then("Photo array has length {int}")
+  public void photoArrayHasLength(int length) {
+    photoList = response.then().spec(resOk()).extract().response().as(Photo[].class);
+    Assert.assertEquals(photoList.length, length);
+  }
+
+  @Then("Album array has length {int}")
+  public void albumArrayHasLength(int length) {
+    albumList = response.then().spec(resOk()).extract().response().as(Album[].class);
+    Assert.assertEquals(albumList.length, length);
+  }
+
+  @Then("Todo array has length {int}")
+  public void todoArrayHasLength(int length) {
+    toDoList = response.then().spec(resOk()).extract().response().as(ToDo[].class);
+    Assert.assertEquals(toDoList.length, length);
+  }
+
+
+  //Method verifies all userIds in the list of posts are correct
   @Then("All posts have userId {int}")
   public void allPostsHaveUserId(int userId) {
-    for(Post post:postList) {
+    for (Post post : postList) {
       Assert.assertEquals(post.getUserId(), userId);
     }
   }
 }
+
+
+
+
+
+
